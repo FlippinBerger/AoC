d058361289ee75991b4f8fe4effa1d88c7537647
Chris Berger <flippinberger@gmail.com>
2024-12-03 23:39:02 -0700

    Prep Day 5

---
+++ 2024/swift/Sources/Day05.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  AdventOfCode
+//
+//  Created by Chris Berger on 12/1/24.
+//
+
+import Foundation
+
+struct Day05: AdventDay {
+  var data: String
+ 
+  func part1() -> Any {
+    return 0
+  }
+  
+  func part2() -> Any {
+    return 0
+  }
+}
